8e60b60edebe575bc4c0365ba2debeda99b91e83
Ernest Ng <eyn5@cornell.edu>
2025-01-28 14:02:37 -0500

    First stab at defining types for the Bril AST

---
+++ lib/ast.ml
@@ -0,0 +1,66 @@
+(** All [label]s are just strings *)
+type label = string
+
+(** All arguments are strings *)
+type arg = string
+
+(** Primitive types (int or bools) *)
+type ty =
+  | TyInt
+  | TyBool
+
+(** Literal values (int & bool values) *)
+type literal =
+  | LitInt of int
+  | LitBool of bool
+
+(** A {i destination variable} is a pair consisting of 
+    the variable name & the vaariable's type *)
+type dest = string * ty
+
+(** Binary operators *)
+type binop =
+  | Add
+  | Mul
+  | Sub
+  | Div
+  | Eq
+  | Lt
+  | Gt
+  | Le
+  | Ge
+
+(** Unary operators *)
+type unop =
+  | Not
+  | And
+  | Or
+
+(** The type of Bril instructions *)
+type instr =
+  | Label of label
+  | Const of dest * literal
+  | Binop of dest * binop * arg * arg
+  | Unop of dest * unop * arg
+  | Jmp of label
+  | Br of arg * label * label
+  | Ret of arg option
+  | Print of arg list
+  | Nop
+  | Call of dest * string * arg list
+
+(** The type of a Bril function: 
+{[
+  {
+    "name": "<string>",
+    "args": [{"name": "<string>", "type": <Type>}, ...]?,
+    "type": <Type>?,
+    "instrs": [<Instruction>, ...]
+  } 
+]} *)
+type func = {
+  name : string;
+  args : (string * ty) list;
+  ret_type : ty option;
+  instrs : (string * instr list) list;
+}
--- lib/cfg.ml
+++ lib/cfg.ml
@@ -10,6 +10,27 @@ let load_json () : Yojson.Basic.t =
 let ($?) (json : Yojson.Basic.t) (key : string) : Yojson.Basic.t = 
   member key json   
 
+(** Determines if a JSON object contains a key *)  
+let contains_key (json : Yojson.Basic.t) (key : string) : bool = 
+  match json $? key with 
+  | `Null -> false 
+  | _ -> true   
+  
+(* let form_blocks body = 
+  List.fold_left (fun acc) [] body *)
+
+(** Creates labels for a list of blocks 
+    (generating fresh labels when necessary) *)  
+(* let mk_block_map blocks = 
+  List.map (fun block -> 
+    if contains_key block 
+    
+    
+    ) blocks  *)
+
+
+
+
 let () = 
   let json = load_json () in 
   let functions = json $? "functions" in 
