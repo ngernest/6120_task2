568f2c7962ca6badc9282afc5fb035c658afa058
Ernest Ng <eyn5@cornell.edu>
2025-01-28 15:01:04 -0500

    Convert to/from opcodes and binary/unary ops

--- lib/syntax.ml
+++ lib/syntax.ml
@@ -21,6 +21,10 @@ type literal =
     the variable name & the variable's type *)
 type dest = string * ty
 
+(* -------------------------------------------------------------------------- *)
+(*                              Binary Operators                              *)
+(* -------------------------------------------------------------------------- *)
+
 (** Binary operators *)
 type binop =
   | Add
@@ -32,12 +36,56 @@ type binop =
   | Gt
   | Le
   | Ge
+  | And
+  | Or
+
+let binop_opcode_map : (string * binop) list =
+  [
+    ("add", Add);
+    ("mul", Mul);
+    ("sub", Sub);
+    ("div", Div);
+    ("eq", Eq);
+    ("lt", Lt);
+    ("gt", Gt);
+    ("le", Le);
+    ("ge", Ge);
+    ("and", And);
+    ("or", Or);
+  ]
+
+(** Determines if an opcode represents a binary operator *)
+let is_binop (opcode : string) : bool =
+  let binop_opcodes = List.map ~f:fst binop_opcode_map in
+  List.mem opcode ~set:binop_opcodes
+
+(** Converts a string to a [binop] *)
+let binop_of_string (opcode : string) : binop =
+  List.assoc opcode binop_opcode_map
+
+(* -------------------------------------------------------------------------- *)
+(*                               Unary operators                              *)
+(* -------------------------------------------------------------------------- *)
 
 (** Unary operators *)
 type unop =
   | Not
-  | And
-  | Or
+  | Id
+
+(** Maps each unary operator's opcode to the corresponding [unop] *)
+let unop_opcode_map : (string * unop) list = [ ("not", Not); ("id", Id) ]
+
+(** Determines if an opcode represents a binary operator *)
+let is_unop (opcode : string) : bool =
+  let unop_opcodes = List.map ~f:fst unop_opcode_map in
+  List.mem opcode ~set:unop_opcodes
+
+(** Converts a string to an [unop] *)
+let unop_of_string (opcode : string) : unop = List.assoc opcode unop_opcode_map
+
+(* -------------------------------------------------------------------------- *)
+(*                                Instructions                                *)
+(* -------------------------------------------------------------------------- *)
 
 (** The type of Bril instructions *)
 type instr =
@@ -69,23 +117,24 @@ type func = {
 }
 
 (******************************************************************************)
+open Yojson.Basic.Util
 
 (** Retrieves the contents of the ["args"] field in a JSON object 
     as a list of strings  *)
 let get_args (json : Yojson.Basic.t) : arg list =
   let args_list = Helpers.list_of_json (json $! "args") in
-  List.map ~f:Yojson.Basic.Util.to_string args_list
+  List.map ~f:to_string args_list
 
 (** Retrieves the contents of the ["fields"] field in a JSON object 
     as a list of strings  *)
 let get_labels (json : Yojson.Basic.t) : arg list =
   let args_list = Helpers.list_of_json (json $! "arlabelsgs") in
-  List.map ~f:Yojson.Basic.Util.to_string args_list
+  List.map ~f:to_string args_list
 
 let instr_of_json (json : Yojson.Basic.t) : instr =
   match json $! "label" with
   | `String label -> Label label
-  | `Null -> failwith "TODO"
-  | _ ->
-    failwith
-      (Printf.sprintf "Invalid JSON : %s" (Yojson.Basic.Util.to_string json))
+  | `Null ->
+    let opcode : string = to_string (json $! "op") in
+    if is_binop opcode then failwith "TODO (Binop (_, _))" else failwith "TODO"
+  | _ -> failwith (Printf.sprintf "Invalid JSON : %s" (to_string json))
